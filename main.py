55f1c49472202dc41112cfb19c775ac80c3a50bb
Wing Light <wing_light@hotmail.com>
2024-05-20 20:38:56 +0800

    fix var processed_count

--- main.py
+++ main.py
@@ -11,67 +11,67 @@ from tkinter import filedialog, ttk, messagebox
 
 install_whisper()
 
-def count_files():
-    file_count = 0
-    for dir_path in VIDEO_DIRS:
-        for root, _, files in os.walk(dir_path):
-            for file in files:
-                _, file_ext = os.path.splitext(file)
-                if file_ext in VIDEO_SUFFIXES and not file.startswith("."):
-                    file_count += 1
-    return file_count
+# def count_files():
+#     file_count = 0
+#     for dir_path in VIDEO_DIRS:
+#         for root, _, files in os.walk(dir_path):
+#             for file in files:
+#                 _, file_ext = os.path.splitext(file)
+#                 if file_ext in VIDEO_SUFFIXES and not file.startswith("."):
+#                     file_count += 1
+#     return file_count
 
-total_files = count_files()
-start_time = time.time()
-processed_count = 0
+# total_files = count_files()
+# start_time = time.time()
+# processed_count = 0
 
-def process_file(file_path, base_dir):
-    file_name = os.path.basename(file_path)
-    _, file_ext = os.path.splitext(file_name)
-    if file_ext in VIDEO_SUFFIXES and not file_name.startswith("."):
-        print(f'开始处理文件：{file_path}')
-        try:
-            base_name = os.path.splitext(file_name)[0]
-            wav_file = os.path.join(base_dir, f'{base_name}.wav')
-            src_lang = TRANSLATE_CONFIG['source_language']
-            srt_file = os.path.join(base_dir, f'{base_name}.{src_lang}.srt')
+# def process_file(file_path, base_dir):
+#     file_name = os.path.basename(file_path)
+#     _, file_ext = os.path.splitext(file_name)
+#     if file_ext in VIDEO_SUFFIXES and not file_name.startswith("."):
+#         print(f'开始处理文件：{file_path}')
+#         try:
+#             base_name = os.path.splitext(file_name)[0]
+#             wav_file = os.path.join(base_dir, f'{base_name}.wav')
+#             src_lang = TRANSLATE_CONFIG['source_language']
+#             srt_file = os.path.join(base_dir, f'{base_name}.{src_lang}.srt')
 
-            if not os.path.exists(srt_file):
-                srt_file = os.path.join(base_dir, f'{base_name}.{src_lang}')
-                if not os.path.exists(wav_file):
-                    extract_audio(file_path, wav_file)
-                    print('完成音频文件提取，准备生成字幕文件')
+#             if not os.path.exists(srt_file):
+#                 srt_file = os.path.join(base_dir, f'{base_name}.{src_lang}')
+#                 if not os.path.exists(wav_file):
+#                     extract_audio(file_path, wav_file)
+#                     print('完成音频文件提取，准备生成字幕文件')
 
-                cmd = f'./whisper.cpp/main -m ./whisper.cpp/models/ggml-{WHISPER_MODEL}.bin -f "{wav_file}" -osrt -of "{srt_file}"'
-                subprocess.run(cmd, shell=True, check=True)
+#                 cmd = f'./whisper.cpp/main -m ./whisper.cpp/models/ggml-{WHISPER_MODEL}.bin -f "{wav_file}" -osrt -of "{srt_file}"'
+#                 subprocess.run(cmd, shell=True, check=True)
 
-                global processed_count
-                processed_count = processed_count + 1
+#                 global processed_count
+#                 processed_count = processed_count + 1
 
-            if os.path.exists(wav_file):
-                os.remove(wav_file)
-                print(f'删除wav文件 {wav_file}')
+#             if os.path.exists(wav_file):
+#                 os.remove(wav_file)
+#                 print(f'删除wav文件 {wav_file}')
 
-        except Exception as e:
-            print(f'执行出错 {e}')
+#         except Exception as e:
+#             print(f'执行出错 {e}')
 
-def traverse_dirs(dir_paths, level=1):
-    if level > 4:
-        return
+# def traverse_dirs(dir_paths, level=1):
+#     if level > 4:
+#         return
 
-    for dir_path in dir_paths:
-        for entry in os.listdir(dir_path):
-            entry_path = os.path.join(dir_path, entry)
-            if os.path.isdir(entry_path):
-                traverse_dirs([entry_path], level + 1)
-            else:
-                process_file(entry_path, dir_path)
-                # 记时、刷新进度\
-                if processed_count > 0:
-                    elapsed_time = time.time() - start_time
-                    remaining_time = (elapsed_time / processed_count) * (total_files - processed_count)
-                    progress = processed_count / total_files * 100
-                    print(f"\r进度: {progress:.2f}% 剩余时间: {time.strftime('%H:%M:%S', time.gmtime(remaining_time))}", end="")
+#     for dir_path in dir_paths:
+#         for entry in os.listdir(dir_path):
+#             entry_path = os.path.join(dir_path, entry)
+#             if os.path.isdir(entry_path):
+#                 traverse_dirs([entry_path], level + 1)
+#             else:
+#                 process_file(entry_path, dir_path)
+#                 # 记时、刷新进度\
+#                 if processed_count > 0:
+#                     elapsed_time = time.time() - start_time
+#                     remaining_time = (elapsed_time / processed_count) * (total_files - processed_count)
+#                     progress = processed_count / total_files * 100
+#                     print(f"\r进度: {progress:.2f}% 剩余时间: {time.strftime('%H:%M:%S', time.gmtime(remaining_time))}", end="")
 
 # traverse_dirs(VIDEO_DIRS)         
 
@@ -171,6 +171,7 @@ class App:
             return
 
         start_time = time.time()
+        processed_count = 0
         for index, video_file in enumerate(video_files):
             if self.stop_flag:
                 break
@@ -193,7 +194,6 @@ class App:
                     cmd = f'./whisper.cpp/main -m ./whisper.cpp/models/ggml-{whisper_model}.bin -f "{wav_file}" -osrt -of "{srt_file}"'
                     subprocess.run(cmd, shell=True, check=True)
 
-                    global processed_count
                     processed_count = processed_count + 1
 
                 if os.path.exists(wav_file):
@@ -209,7 +209,7 @@ class App:
             remaining_time = estimated_total_time - elapsed_time
 
             self.progress_var.set(progress)
-            self.progress_label.config(text=f'进度: {progress:.2f}% 剩余时间: {time.strftime("%H:%M:%S", time.gmtime(remaining_time))}')
+            self.progress_label.config(text=f'{index}/{total_files} 进度: {progress:.2f}% 剩余时间: {time.strftime("%H:%M:%S", time.gmtime(remaining_time))} \n{video_file}')
             self.root.update_idletasks()
 
         self.start_button.config(state='normal')
